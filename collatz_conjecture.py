2d6f4771ef14aef08d6b2faec63b3da58de8a3e0
Azaan Rais <75561979+AzaanRais@users.noreply.github.com>
2023-01-04 12:35:27 +0530

    Added explanatory comments to collatz_conjecture.py

--- collatz_conjecture.py
+++ collatz_conjecture.py
@@ -1,5 +1,13 @@
 import matplotlib.pyplot as plt
 
+'''
+this program creates the famous Collatz Conjecture, which is also known as 3n+1,
+any random starting number is taken, if it is even, it is divided by 2, else it is multiplied by 3 and 1 is added to the product - this cycle is repeated and for every number, this cycle eventually gets stuck in a loop of 4-2-1 (this has been tested till 2^68, but this conjecture is not proven true yet)
+
+example:
+3 -> 3, 10, 5, 16, 8, 4, 2, 1....
+'''
+
 def main():
     inp = int(input("Enter number: "))
     if inp >= 1:
